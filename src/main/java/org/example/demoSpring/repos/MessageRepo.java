a56f24358258f1f04f0a5a8459d991f47812dc36
alex3707 <alex3707@mail.ru>
2022-08-21 19:55:27 +0300

    2 lesson finish

---
+++ src/main/java/org/example/demoSpring/repos/MessageRepo.java
@@ -0,0 +1,10 @@
+package org.example.demoSpring.repos;
+
+import org.example.demoSpring.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Integer> {
+    List<Message> findByTag(String tag);
+}
